3449bf85a6cddcb39cc226289e12286426b89d53
citharus <derangler2004@gmail.com>
2021-02-23 22:43:40 +0100

    Added Exception
    Signed-off-by: citharus <derangler2004@gmail.com>

--- CPAW/exceptions.py
+++ CPAW/exceptions.py
@@ -17,6 +17,11 @@ class InvalidLoginException(Exception):
         super().__init__("Invalid Login Credentials")
 
 
+class InvalidSessionTokenException(Exception):
+    def __init__(self):
+        super().__init__("Invalid session token")
+
+
 class InvalidServerResponseException(Exception):
     def __init__(self, response: dict):
         super().__init__("Invalid Server Response: " + json.dumps(response))
