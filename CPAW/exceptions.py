b3509e9f0c691a7b75d93e85f47ba38682b9db6a
citharus <derangler2004@gmail.com>
2021-02-23 19:33:50 +0100

    Added UnknownMicroserviceError
    Signed-off-by: citharus <derangler2004@gmail.com>

--- CPAW/exceptions.py
+++ CPAW/exceptions.py
@@ -4,3 +4,8 @@ import json
 class InvalidServerResponseError(Exception):
     def __init__(self, response: dict):
         super().__init__("Invalid Server Response: " + json.dumps(response))
+
+
+class UnknownMicroserviceError(Exception):
+    def __init__(self, microservice: str):
+        super().__init__("Unknown Microservice: " + microservice)
