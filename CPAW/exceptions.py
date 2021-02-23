270457d2825ac4537b8644f5eb433af6c2f94f14
citharus <derangler2004@gmail.com>
2021-02-23 21:42:55 +0100

    Added Exception
    Signed-off-by: citharus <derangler2004@gmail.com>

--- CPAW/exceptions.py
+++ CPAW/exceptions.py
@@ -12,6 +12,11 @@ class LoggedOutException(Exception):
         super().__init__("Endpoint can only be used while client is logged in.")
 
 
+class InvalidLoginException(Exception):
+    def __init__(self):
+        super().__init__("Invalid Login Credentials")
+
+
 class InvalidServerResponseException(Exception):
     def __init__(self, response: dict):
         super().__init__("Invalid Server Response: " + json.dumps(response))
