3e9f21b135cba6a2d78d296949a67391914106f9
citharus <derangler2004@gmail.com>
2021-02-24 15:17:16 +0100

    Added creation of devices

--- CPAW/microservices/hardware.py
+++ CPAW/microservices/hardware.py
@@ -1,6 +1,20 @@
+from typing import List
+
+from CPAW import Client
+from CPAW.microservices import Device
+
+
 class Hardware:
-    def __init__(self, data: dict):
+    def __init__(self, client: Client, data: dict) -> None:
+        self.client: Client = client
         self.uuid: str = data["uuid"]
         self.device: str = data["device_uuid"]
         self.hardware_element: str = data["hardware_element"]
         self.hardware_type: str = data["hardware_type"]
+
+    def create(self, gpu: List[str], cpu: List[str], mainboard: str, ram: List[str], disk: List[str],
+               processorCooler: List[str], powerPack: str, case: str) -> Device:
+        response: dict = self.client.microservice("device", ["device", "create"], gpu=gpu, cpu=cpu, mainboard=mainboard,
+                                                  ram=ram, disk=disk, processorCooler=processorCooler, case=case,
+                                                  powerPack=powerPack)
+        return Device(self.client, response)
