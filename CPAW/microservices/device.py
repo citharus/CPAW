994fceae923d6ea5c4bebe3a15740f862a841c9c
citharus <derangler2004@gmail.com>
2021-02-24 13:09:56 +0100

    Added file listing

--- CPAW/microservices/device.py
+++ CPAW/microservices/device.py
@@ -1,11 +1,12 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
 
 from CPAW import Client
+from CPAW.microservices import File
 
 
 class Device:
     def __init__(self, client: Client, data: dict) -> None:
-        self.microservice = client.microservice
+        self.client = client
         self.uuid: str = data["uuid"]
         self.name: str = data["name"]
         self.owner: str = data["owner"]
@@ -13,23 +14,28 @@ class Device:
         self.starter_device: bool = bool(data["starter_device"])
 
     def info(self) -> Dict:
-        return self.microservice("device", ["device", "info"], device_uuid=self.uuid)
+        return self.client.microservice("device", ["device", "info"], device_uuid=self.uuid)
 
     def ping(self) -> bool:
-        return self.microservice("device", ["device", "ping"], device_uuid=self.uuid)["online"]
+        return self.client.microservice("device", ["device", "ping"], device_uuid=self.uuid)["online"]
 
     def all(self) -> List[Dict[str, Union[str, bool]]]:
-        return self.microservice("device", ["device", "all"])["devices"]
+        return self.client.microservice("device", ["device", "all"])["devices"]
 
     def power(self) -> bool:
-        self.powered_on = self.microservice("device", ["device", "power"], device_uuid=self.uuid)["powered_on"]
+        self.powered_on = self.client.microservice("device", ["device", "power"], device_uuid=self.uuid)["powered_on"]
         return self.powered_on
 
     def change_name(self, name: str) -> str:
-        return self.microservice("device", ["device", "change_name"], device_uuid=self.uuid, name=name)["name"]
+        return self.client.microservice("device", ["device", "change_name"], device_uuid=self.uuid, name=name)["name"]
 
     def delete(self) -> bool:
-        return self.microservice("device", ["device", "delete"], device_uuid=self.uuid)["ok"]
+        return self.client.microservice("device", ["device", "delete"], device_uuid=self.uuid)["ok"]
 
     def exists(self) -> bool:
-        return self.microservice("device", ["exist"], device_uuid=self.uuid)["exist"]
+        return self.client.microservice("device", ["exist"], device_uuid=self.uuid)["exist"]
+
+    def files(self, parent_dir_uuid: Optional[str] = None) -> List[File]:
+        response: list = self.client.microservice("device", ["file", "all"], device_uuid=self.uuid,
+                                                  parent_dir_uuid=parent_dir_uuid)["files"]
+        return [File(self.client, file) for file in response]
