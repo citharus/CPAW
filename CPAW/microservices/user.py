982a500517395fca8e1477ba853ea576a1c41c8c
citharus <derangler2004@gmail.com>
2021-02-24 17:23:23 +0100

    Added Docstrings

--- CPAW/microservices/user.py
+++ CPAW/microservices/user.py
@@ -5,18 +5,33 @@ from CPAW.microservices import Device
 
 
 class User:
+    """The class of an Cryptic Game user account"""
     def __init__(self, client: Client) -> None:
+        """
+        :param Client client: The client used by the user
+        """
         self.client: Client = client
         data: dict = client.info()
         self.name: str = data["name"]
         self.uuid: str = data["uuid"]
 
-    def delete_devices(self) -> bool:
-        return self.client.microservice("device", ["delete_user"], user_uuid=self.uuid)["ok"]
+    def delete_devices(self) -> None:
+        """Delete all `Devices` of an user"""
+        self.client.microservice("device", ["delete_user"], user_uuid=self.uuid)
 
     def devices(self) -> List[Device]:
+        """
+        List all `Devices` of the user
+        :return: List of Devices
+        :rtype: list[Device]
+        """
         response: list = self.client.microservice("device", ["device", "all"])["devices"]
         return [Device(self.client, device) for device in response]
 
     def spot(self) -> Device:
+        """
+        Find a random `Device`.
+        :return: A random device
+        :rtype: Device
+        """
         return Device(self.client, self.client.microservice("device", ["spot"]))
