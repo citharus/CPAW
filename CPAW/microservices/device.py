8ddba63ef9d07b806b7fa8fb7dbaccdea953e607
citharus <derangler2004@gmail.com>
2021-02-24 09:23:19 +0100

    Added Device class
    Signed-off-by: citharus <derangler2004@gmail.com>

---
+++ CPAW/microservices/device.py
@@ -0,0 +1,11 @@
+from CPAW import Client
+
+
+class Device:
+    def __init__(self, client: Client, data: dict) -> None:
+        self.microservice = client.microservice
+        self.uuid: str = data["uuid"]
+        self.name: str = data["name"]
+        self.owner: str = data["owner"]
+        self.powered_on: bool = bool(data["powered_on"])
+        self.starter_device: bool = bool(data["starter_device"])
