b29b8c0b93cd0b185f8fcca7eeca893ba6b0c963
citharus <derangler2004@gmail.com>
2021-02-24 13:15:09 +0100

    Added delete method

--- CPAW/microservices/file.py
+++ CPAW/microservices/file.py
@@ -3,7 +3,7 @@ from CPAW import Client
 
 class File:
     def __init__(self, client: Client, data: dict) -> None:
-        self.microservice = client.microservice
+        self.client = client
         self.uuid: str = data["uuid"]
         self.device: str = data["device"]
         self.filename: str = data["filename"]
@@ -12,11 +12,14 @@ class File:
         self.is_directory: bool = bool(data["id_directory"])
 
     def move(self, new_parent_dir_uuid: str, new_filename: str) -> None:
-        response: dict = self.microservice("file", ["file", "move"], new_parent_dir_uuid=new_parent_dir_uuid,
-                                           new_filename=new_filename)
+        response: dict = self.client.microservice("device", ["file", "move"], new_parent_dir_uuid=new_parent_dir_uuid,
+                                                  new_filename=new_filename)
         self.filename = response["new_filename"]
         self.parent_dir_uuid = response["new_parent_dir_uuid"]
 
     def update(self, content: str) -> None:
-        self.content = self.microservice("file", ["file", "update"], device_uuid=self.device, file_uuid=self.uuid,
-                                         content=content)["content"]
+        self.content = self.client.microservice("device", ["file", "update"], device_uuid=self.device, content=content,
+                                                file_uuid=self.uuid)["content"]
+
+    def delete(self) -> None:
+        self.client.microservice("device", ["file", "delete"], device_uuid=self.device, file_uuid=self.uuid)
