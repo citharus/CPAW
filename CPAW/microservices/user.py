06e3aef64135cdf57a91e76c00c7edd2ae3fa1a3
citharus <derangler2004@gmail.com>
2021-02-25 17:59:28 +0100

    Closed #4

--- CPAW/microservices/user.py
+++ CPAW/microservices/user.py
@@ -35,3 +35,23 @@ class User:
         :rtype: Device
         """
         return Device(self.client, self.client.microservice("device", ["spot"]))
+
+    def create(self, gpu: List[str], cpu: List[str], mainboard: str, ram: List[str], disk: List[str],
+               processorCooler: List[str], powerPack: str, case: str) -> Device:
+        """
+
+        :param list[str] gpu: The names of the graphics processing units
+        :param list[str] cpu: The names of the central processing units
+        :param str mainboard: The name of the motherboard
+        :param list[str] ram: The names of the memory units
+        :param list[str] disk: The names of the disks
+        :param list[str] processorCooler: The names of the cpu coolers
+        :param str powerPack: The name of the power pack
+        :param str case: The name of the case
+        :return: A new device
+        :rtype: Device
+        """
+        response: dict = self.client.microservice("device", ["device", "create"], gpu=gpu, cpu=cpu, mainboard=mainboard,
+                                                  ram=ram, disk=disk, processorCooler=processorCooler, case=case,
+                                                  powerPack=powerPack)
+        return Device(self.client, response)
