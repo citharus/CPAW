d4c41be53e8fb0f5b354f7799dac25bcd7f96c05
citharus <derangler2004@gmail.com>
2021-02-26 22:46:05 +0100

    Added docstring

--- CPAW/models/miner.py
+++ CPAW/models/miner.py
@@ -3,7 +3,12 @@ from CPAW.models import Service
 
 
 class Miner(Service):
+    """The representation of the Miner service."""
     def __init__(self, client: Client, data: dict) -> None:
+        """
+        :param Client client: The client used by the user
+        :param dict data: The data of the Miner service
+        """
         super(Miner, self).__init__(client, data)
         self.wallet: str = data["wallet"]
         self.power: int = int(data["power"])
