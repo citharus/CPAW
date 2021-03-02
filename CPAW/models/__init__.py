cc5eb70903870a274c59f76acd2df5c6216f44f4
citharus <citharus@protonmail.com>
2021-03-02 11:48:28 +0100

    ♻️ Change import

--- CPAW/models/__init__.py
+++ CPAW/models/__init__.py
@@ -2,5 +2,5 @@ from .device import Device
 from .file import File
 from .hardware import Hardware
 from .miner import Miner
-from .service import *
+from .service import Service, BruteforceService, PortscanService, SSHService, TelnetService
 from .user import User
