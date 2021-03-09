9d5125ae7efae80134aca59a5a6ea19ec06af5ad
citharus <citharus@protonmail.com>
2021-03-09 17:28:31 +0100

    ♻️ Made key argument optional

--- CPAW/models/wallet.py
+++ CPAW/models/wallet.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from CPAW import Client
 
 
@@ -5,9 +6,8 @@ class Wallet:
     def __init__(self, client: Client, data: dict) -> None:
         self.client: Client = client
         self.uuid: str = data["source_uuid"]
-        self.key: str = data["key"]
-        self.owner: str = data["user_uuid"]
+        self.key: Optional[str] = data["key"]
+        self.owner: Optional[str] = data["user_uuid"]
 
-    @property
-    def amount(self) -> float:
-        return self.client.microservice("currency", ["get"], source_uuid=self.uuid, key=self.key)["amount"]
+    def amount(self, key: str) -> float:
+        return self.client.microservice("currency", ["get"], source_uuid=self.uuid, key=key)["amount"]
