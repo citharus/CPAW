225236a83bb021fc5975681e74c8c96d6ca42977
citharus <citharus@protonmail.com>
2021-03-31 12:51:22 +0200

    ✅ Added client logout test case

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,3 +7,10 @@ class TestClient(Test):
         response: str = self.client.login()
         self.assertRegex(response, r"[a-z0-9]{8}(\-[a-z0-9]{4}){3}\-[a-z0-9]{12}")
         self.assertTrue(self.client.logged_in)
+
+    def test_logout(self) -> None:
+        self.client.logout()
+        print(self.client.logged_in)
+        self.assertFalse(self.client.logged_in)
+        self.assertIsNone(self.client.websocket)
+        self.client.login()
