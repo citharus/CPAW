5556b74757a3fb2b75233070ccd6e7b3857f2575
citharus <citharus@protonmail.com>
2021-03-31 14:54:25 +0200

    ✅ Removed test cases

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,20 +4,6 @@ from tests.test import Test
 
 
 class TestClient(Test):
-    def test_login(self) -> None:
-        self.client.logout()
-        response: str = self.client.login()
-
-        self.assertRegex(response, r"[a-z0-9]{8}(\-[a-z0-9]{4}){3}\-[a-z0-9]{12}")
-        self.assertTrue(self.client.logged_in)
-
-    def test_logout(self) -> None:
-        self.client.logout()
-
-        self.assertFalse(self.client.logged_in)
-        self.assertIsNone(self.client.websocket)
-
-        self.client.login()
 
     @patch("CPAW.client.Client.request")
     def test_request(self, mocked_method) -> None:
