2096fe54a12de6f05bddbf601b1f83f1f257d3da
citharus <citharus@protonmail.com>
2021-03-31 15:01:21 +0200

    ✅ Added microservice test case

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,7 +4,6 @@ from tests.test import Test
 
 
 class TestClient(Test):
-
     @patch("CPAW.client.Client.request")
     def test_request(self, mocked_method) -> None:
         response: dict = self.client.request({"action": "info"})
@@ -13,5 +12,16 @@ class TestClient(Test):
         self.assertEqual(
             mocked_method.call_args_list,
             [call({"action": "info"})]
+        )
+        self.assertEqual(mocked_method.return_value, response)
+
+    @patch("CPAW.client.Client.microservice")
+    def test_microservice(self, mocked_method) -> None:
+        response: dict = self.client.microservice("device", ["spot"])
+
+        self.assertTrue(mocked_method.called)
+        self.assertEqual(
+            mocked_method.call_args_list,
+            [call("device", ["spot"])]
         )
         self.assertEqual(mocked_method.return_value, response)
