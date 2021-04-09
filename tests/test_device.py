90fb5c444d80bec741bbd60c43c6e5833780fb37
citharus <citharus@protonmail.com>
2021-04-09 16:33:23 +0200

    ✅ Changed Device data env variable

--- tests/test_device.py
+++ tests/test_device.py
@@ -4,8 +4,7 @@ from tests.test import Test, getenv
 
 class TestDevice(Test):
     def setUp(self) -> None:
-        self.device: Device = Device(self.client, {'uuid': getenv("TEST_DEVICE_UUID"),
-                                                   'name': getenv("TEST_DEVICE_NAME"), 'starter_device': True})
+        self.device: Device = Device(self.client, eval(getenv("TEST_DEVICE")))
 
     def test_power(self) -> None:
         response: bool = self.device.power
