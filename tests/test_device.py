e7f31de56c5417f8399c14ec16787ec72955a6ba
citharus <citharus@protonmail.com>
2021-04-09 13:47:02 +0200

    ✅ Added power test case

--- tests/test_device.py
+++ tests/test_device.py
@@ -6,3 +6,8 @@ class TestDevice(Test):
     def setUp(self) -> None:
         self.device: Device = Device(self.client, {'uuid': getenv("TEST_DEVICE_UUID"),
                                                    'name': getenv("TEST_DEVICE_NAME"), 'starter_device': True})
+
+    def test_power(self) -> None:
+        response: bool = self.device.power
+
+        self.assertIsInstance(response, bool)
