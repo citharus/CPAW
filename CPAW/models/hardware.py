bb6b6faceb03f65019dde8e11ce5cdfce1604914
citharus <citharus@protonmail.com>
2021-04-09 14:37:42 +0200

    🏗 Changed to use the base model

--- CPAW/models/hardware.py
+++ CPAW/models/hardware.py
@@ -1,16 +1,39 @@
 from CPAW import Client
+from CPAW.models import BaseModel
 
 
-class Hardware:
+class Hardware(BaseModel):
     """The representation of a hardware part"""
     def __init__(self, client: Client, data: dict) -> None:
         """
         :param Client client: The client used by the user
         :param dict data: The data of the hardware part
         """
-        self.client: Client = client
-        self._data: dict = data
-        self.uuid: str = data["uuid"]
-        self.device: str = data["device_uuid"]
-        self.name: str = data["hardware_element"]
-        self.type: str = data["hardware_type"]
+        super().__init__(client, data)
+
+    @property
+    def device(self) -> str:
+        """
+        Return the device uuid of the hardware component.
+        :return: The device uuid
+        :rtype: str
+        """
+        return self._data["device_uuid"]
+
+    @property
+    def name(self) -> str:
+        """
+        Return the name of the hardware component.
+        :return: The name
+        :rtype: str
+        """
+        return self._data["hardware_element"]
+
+    @property
+    def type(self) -> str:
+        """
+        Return the type of the hardware component.
+        :return: The type
+        :rtype: str
+        """
+        return self._data["hardware_type"]
