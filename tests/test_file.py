ffb9c0b02ae426d3e665940d16d3f3de0b0c86da
citharus <citharus@protonmail.com>
2021-04-10 15:09:20 +0200

    ✅ Added file content test case

--- tests/test_file.py
+++ tests/test_file.py
@@ -6,3 +6,8 @@ class TestFile(Test):
     def setUp(self) -> None:
         self.device: Device = Device(self.client, eval(getenv("TEST_DEVICE")))
         self.file: File = self.device.files[0]
+
+    def test_content(self) -> None:
+        self.file.content = "Test"
+
+        self.assertEqual(self.file.content, self.file._data["content"])
