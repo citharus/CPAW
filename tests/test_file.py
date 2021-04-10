eb8cde512809d63cd5659893c9e8b7984efcb498
citharus <citharus@protonmail.com>
2021-04-10 15:03:42 +0200

    🚧 Added Files test suite

---
+++ tests/test_file.py
@@ -0,0 +1,7 @@
+from CPAW.models import File, Device
+from tests.test import Test, getenv
+
+
+class TestFile(Test):
+    def setUp(self) -> None:
+        self.device: Device = Device(self.client, eval(getenv("TEST_DEVICE")))
