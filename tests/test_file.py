f3877c7b1b7bdfb67b3003c31bb0b08a3b3a3add
citharus <citharus@protonmail.com>
2021-04-10 15:06:55 +0200

    🚧 Added test File

--- tests/test_file.py
+++ tests/test_file.py
@@ -5,3 +5,4 @@ from tests.test import Test, getenv
 class TestFile(Test):
     def setUp(self) -> None:
         self.device: Device = Device(self.client, eval(getenv("TEST_DEVICE")))
+        self.file: File = self.device.files[0]
