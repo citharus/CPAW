86958054d1227071934e6682ccce4ef7286c006e
citharus <citharus@protonmail.com>
2021-05-06 18:57:45 +0200

    ✨ Added transaction model

--- CPAW/models/__init__.py
+++ CPAW/models/__init__.py
@@ -4,8 +4,9 @@ from CPAW.models.file import File
 from CPAW.models.hardware import Hardware
 from CPAW.models.miner import Miner
 from CPAW.models.service import Service, BruteforceService, PortscanService, SSHService, TelnetService
+from CPAW.models.transaction import Transaction
 from CPAW.models.user import User
 from CPAW.models.wallet import Wallet
 
 __all__ = ["Device", "File", "Hardware", "Miner", "Service", "BruteforceService", "PortscanService", "SSHService",
-           "TelnetService", "User", "Wallet", "BaseModel"]
+           "TelnetService", "User", "Wallet", "BaseModel", "Transaction"]
