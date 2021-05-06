134fc72a8df06837eda60ec522b7a650754f8ae6
citharus <citharus@protonmail.com>
2021-05-06 17:15:30 +0200

    ✅ Added info test case

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, call
+
 from CPAW.models import Wallet
 from tests.test import Test, getenv
 
@@ -6,3 +8,13 @@ class TestWallet(Test):
     def setUp(self) -> None:
         self.wallet: Wallet = Wallet(self.client, eval(getenv("TEST_WALLET")))
 
+    @patch("CPAW.models.wallet.Wallet.info")
+    def test_info(self, mocked_method) -> None:
+        response: dict = self.wallet.info(self.wallet.key)
+
+        self.assertTrue(mocked_method.called)
+        self.assertEqual(
+            mocked_method.call_args_list,
+            [call(self.wallet.key)]
+        )
+        self.assertEqual(mocked_method.return_value, response)
